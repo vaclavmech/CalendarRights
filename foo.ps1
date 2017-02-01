091dcce5caed161601a40ff2e5ffda64cdb346d4
mechhvac <mechhvac@corp.contoso.com>
2017-02-01 20:14:23 +0000

    DataGrid added

--- foo.ps1
+++ foo.ps1
@@ -1,7 +1,7 @@
 # mechhvac
 # Tieto
-# some change
 
+# add support for WPF
 Add-Type -AssemblyName presentationframework
 
 #region GUI
@@ -11,15 +11,28 @@ Add-Type -AssemblyName presentationframework
         xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
         xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
         xmlns:local="clr-namespace:PowerShellGUI"
-        Title="Simple calendar rights GUI" Height="391" Width="543">
-    <Grid>
-        <TextBox x:Name="textBox" HorizontalAlignment="Left" Height="21" Margin="35,16,0,0" TextWrapping="Wrap" Text="mechhvac@corp.contoso.com" VerticalAlignment="Top" Width="202"/>
-        <TextBox x:Name="textBoxUser" HorizontalAlignment="Left" Height="21" Margin="35,46,0,0" TextWrapping="Wrap" Text="user" VerticalAlignment="Top" Width="202"/>
-        <TextBox x:Name="outputTextBox" HorizontalAlignment="Left" Height="187" Margin="35,118,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="319" />
-        <Button x:Name="buttonGet" Content="Get!" HorizontalAlignment="Left" Height="20" Margin="270,16,0,0" VerticalAlignment="Top" Width="78"/>
-        <Button x:Name="buttonSet" Content="Set!" HorizontalAlignment="Left" Height="20" Margin="270,46,0,0" VerticalAlignment="Top" Width="78"/>
-        <ComboBox x:Name="comboBox" HorizontalAlignment="Left" Margin="35,81,0,0" VerticalAlignment="Top" Width="120"/>
-        <Label x:Name="labelCalendarPath" Content="[calendar path]" HorizontalAlignment="Left" Height="30" Margin="35,321,0,0" VerticalAlignment="Top" Width="481"/>
+        Title="Simple calendar rights GUI" Height="433" Width="603">
+    <Grid Margin="10,0,0,0" RenderTransformOrigin="0.531,0.507" HorizontalAlignment="Left" Width="563">
+        <Grid.ColumnDefinitions>
+            <ColumnDefinition/>
+            <ColumnDefinition Width="0*"/>
+        </Grid.ColumnDefinitions>
+        <TextBox x:Name="textBox" HorizontalAlignment="Left" Height="21" Margin="73,31,0,0" TextWrapping="Wrap" Text="mechhvac@corp.contoso.com" VerticalAlignment="Top" Width="202"/>
+        <TextBox x:Name="textBoxUser" HorizontalAlignment="Left" Height="21" Margin="73,78,0,0" TextWrapping="Wrap" Text="user" VerticalAlignment="Top" Width="202"/>
+        <Button x:Name="buttonGet" Content="Get!" HorizontalAlignment="Left" Height="20" Margin="335,32,0,0" VerticalAlignment="Top" Width="78"/>
+        <Button x:Name="buttonSet" Content="Set!" HorizontalAlignment="Left" Height="20" Margin="335,79,0,0" VerticalAlignment="Top" Width="78" RenderTransformOrigin="0.577,0"/>
+        <ComboBox x:Name="comboBox" HorizontalAlignment="Left" Margin="73,125,0,0" VerticalAlignment="Top" Width="148" Height="22"/>
+        <Label x:Name="labelCalendarPath" Content="[calendar path]" HorizontalAlignment="Left" Height="30" Margin="73,362,0,0" VerticalAlignment="Top" Width="481"/>
+        <Label x:Name="labelMailbox" Content="Mailbox:" HorizontalAlignment="Left" Margin="73,2,0,0" VerticalAlignment="Top" RenderTransformOrigin="0,3.731" Width="58" Height="35"/>
+        <Label x:Name="labelUser" Content="User:" HorizontalAlignment="Left" Margin="73,52,0,0" VerticalAlignment="Top" Height="26" Width="36"/>
+        <Label x:Name="labelAccessRights" Content="Access rights:" HorizontalAlignment="Left" Height="31" Margin="73,99,0,0" VerticalAlignment="Top" Width="120"/>
+        <DataGrid x:Name="dataGrid" SelectionMode="Single" HorizontalAlignment="Left" Height="187" Margin="73,170,0,0" VerticalAlignment="Top" Width="440" AutoGenerateColumns="False" ColumnWidth="Auto">
+            <DataGrid.Columns>
+                <DataGridTextColumn Header="Folder name" Binding="{Binding FolderName}"/>
+                <DataGridTextColumn Header="User" Binding="{Binding User}"/>
+                <DataGridTextColumn Header="Access rights" Binding="{Binding AccessRights}"/>
+            </DataGrid.Columns>
+        </DataGrid>
     </Grid>
 </Window>
 "@
@@ -35,8 +48,7 @@ $xaml.SelectNodes("//*[@*[contains(translate(name(.),'n','N'),'Name')]]")  | For
     New-Variable  -Name $_.Name -Value $window.FindName($_.Name) -Force
 }
 
-# data
-
+# fill the combo box with Access rights data
 $listItemSource = New-Object System.Collections.ObjectModel.ObservableCollection[string]
 $listItemSource.Add("Reviewer")
 $listItemSource.Add("AvailabilityOnly")
@@ -44,19 +56,18 @@ $listItemSource.Add("Owner")
 $listItemSource.Add("Author")
 $comboBox.ItemsSource = $listItemSource
 $comboBox.SelectedIndex = 0
-
-# end date
-
-
-
-
+# 
 
 #region Events
 $buttonGet.Add_Click({
     try{
         if (-NOT ([string]::IsNullOrEmpty($textBox.Text))){
             Write-Host "Getting the calendar permissions.."
-            $outputTextBox.Text = getCalendarData
+            #$outputTextBox.Text = getCalendarData
+            $dataGrid.clear()
+            $dataGrid.ItemsSource = getCalendarData
+            $dataGrid.IsReadOnly = $true
+            
             $labelCalendarPath.Content = getCalendarFolder
         }
     }catch{
@@ -69,7 +80,7 @@ $buttonSet.Add_Click({
     try{
         if (-NOT ([string]::IsNullOrEmpty($textBoxUser.Text))){
             Write-Host "Setting the calendar permissions.."
-            $calendarData = getCalendarData
+            $calendarData = getCalendarData | Out-String
             Write-Host $calendarData
             Write-Host $textBoxUser.Text
             [bool]$contains = $calendarData.contains($textBoxUser.Text)
@@ -79,12 +90,12 @@ $buttonSet.Add_Click({
             if($contains){
                 $folder = getCalendarFolder
                 Set-MailboxFolderPermission $($folder) -User $textBoxUser.Text -AccessRights $combobox.SelectedItem
-                $outputTextBox.Text = getCalendarData
+                #$outputTextBox.Text = getCalendarData
                 Write-Host "Setting..."
             }else{
                 $folder = getCalendarFolder
                 Add-MailboxFolderPermission $($folder) -User $textBoxUser.Text -AccessRights $combobox.SelectedItem
-                $outputTextBox.Text = getCalendarData
+                #$outputTextBox.Text = getCalendarData
                 Write-Host "Adding..."
             }
         }else {
@@ -97,6 +108,8 @@ $buttonSet.Add_Click({
 
 #endregion Events
 
+
+# supporting functions
 function getCalendarFolder{
     $mailbox = $textbox.Text
     $calendarFolder = ((Get-MailboxFolderStatistics $mailbox | Where-Object {$_.FolderType -like "Calendar"}).Folderpath).Replace('/','\')
@@ -106,8 +119,9 @@ function getCalendarFolder{
 
 function getCalendarData{
     $folder = getCalendarFolder
-    $calendarData = Get-MailboxFolderPermission $($folder) | Out-String
+    $calendarData = Get-MailboxFolderPermission $($folder) | Select-Object foldername, user, @{name="AccessRights";expression={ [string]::join(",",@($_.accessrights)) }}
     $calendarData
 }
+#
 
 $Null = $window.ShowDialog()
